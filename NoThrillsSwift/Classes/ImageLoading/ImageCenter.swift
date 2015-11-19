78e61b569e788df7d69990caef6a4d302a881094
DaveC @ DevedUp <email@devedup.com>
2015-11-19 17:54:15 +0000

    updated unit tests for cancellation

--- NoThrillsSwift/Classes/ImageLoading/ImageCenter.swift
+++ NoThrillsSwift/Classes/ImageLoading/ImageCenter.swift
@@ -25,8 +25,8 @@ class ImageCenter {
 	
     - returns: an operation which can be cancelled, or contains image from cache
     */
-	class func imageForURL(url: NSURL, onNetworkLoad: (UIImage) -> Void) -> (image: UIImage?, operation: ImageLoadOperation?) {
-		let imageOperation = ImageLoadOperation(url: url, onNetworkLoad: onNetworkLoad)
+	class func imageForURL(url: NSURL, onCompletion: (image: UIImage?, cancelled: Bool) -> Void) -> (image: UIImage?, operation: ImageLoadOperation?) {
+		let imageOperation = ImageLoadOperation(url: url, onCompletion: onCompletion)
 		if (imageOperation.image != nil) {
 			return (imageOperation.image, nil)
 		} else {
@@ -41,13 +41,13 @@ class ImageLoadOperation: NSOperation {
 	
 	var image: UIImage?
 	
-	private let onNetworkLoad: (UIImage) -> Void
+	private let onCompletion: (image: UIImage?, cancelled: Bool) -> Void
 	private let url: NSURL
 	private let cache = DiskCache()
 	private let cacheKey: String
 	
-	init(url: NSURL, onNetworkLoad: (UIImage) -> Void) {
-		self.onNetworkLoad = onNetworkLoad
+	init(url: NSURL, onCompletion: (UIImage?, Bool) -> Void) {
+		self.onCompletion = onCompletion
 		self.url = url
 		self.cacheKey = url.cacheKey()
 		if let cachedImage = cache.dataForKey(cacheKey) {
@@ -59,17 +59,24 @@ class ImageLoadOperation: NSOperation {
 	
 	override func main() {
 		guard !self.cancelled else {
+			onCompletion(image: nil, cancelled: true)
 			return
 		}
 		guard self.image == nil else {
 			return
 		}
 		
+		print("Loading image from network from \(self.url.absoluteString)")
 		if let data = try? NSData(contentsOfURL: url, options: NSDataReadingOptions.DataReadingUncached) {
+			if (self.cancelled) {
+				onCompletion(image: nil, cancelled: true)
+				return
+			}
 			if let image = UIImage(data: data) {
 				self.image = image
+				print("Completed loading image from network from \(self.url.absoluteString)")
 				cache.storeData(data, forKey: cacheKey)
-				onNetworkLoad(image)
+				onCompletion(image: image, cancelled: false)
 			}
 		}
 	}
--- NoThrillsSwift/NoThrillsSwiftTests/ImageCenterTest.swift
+++ NoThrillsSwift/NoThrillsSwiftTests/ImageCenterTest.swift
@@ -21,13 +21,13 @@ class ImageCenterTest: XCTestCase {
 		let waitForImageLoad = expectationWithDescription("Expecting an image load")
 		
 		let imageURL = NSURL(string:"https://media2.giphy.com/media/wzXlcBruMmzf2/200_s.gif")!
-		let imageResult = ImageCenter.imageForURL(imageURL) { (imageLoaded) -> Void in
+		let imageResult = ImageCenter.imageForURL(imageURL) { (image, cancelled) -> Void in
 			
 			// Should have an image loaded now
-			XCTAssertNotNil(imageLoaded)
+			XCTAssertNotNil(image)
 			
 			// Try again, but it should now be in the cache
-			let cachedImage = ImageCenter.imageForURL(imageURL, onNetworkLoad: {_ in })
+			let cachedImage = ImageCenter.imageForURL(imageURL, onCompletion: {_ in })
 			XCTAssertNotNil(cachedImage)
 			
 			// Let the test end
@@ -44,5 +44,31 @@ class ImageCenterTest: XCTestCase {
 		waitForExpectationsWithTimeout(100.0, handler:nil)
     }
 
-    
+	
+	func testImageCachingCancellingTheRequest() {
+		let waitForCancellation = expectationWithDescription("Expecting an image load")
+		
+		let imageOne = NSURL(string:"https://media2.giphy.com/media/wzXlcBruMmzf2/200_s.gif")!
+		let imageTwo = NSURL(string:"http://www.accrete.com/3dtextures/More3djayTextures/trees/got3d-tree23.png")!
+		let imageThree = NSURL(string:"https://www.sapere.com/ckeditor_assets/pictures/35/content_oak_tree.png")!
+		
+		// The queue only runs 3, so four and five should be cancelled on time
+		let one = ImageCenter.imageForURL(imageOne, onCompletion: {_ in })
+		let two = ImageCenter.imageForURL(imageTwo, onCompletion: {_ in })
+		let three = ImageCenter.imageForURL(imageThree) { (image, cancelled) -> Void in
+			XCTAssertTrue(cancelled)
+			XCTAssertNil(image)
+			
+			// Let the test end
+			waitForCancellation.fulfill()
+		}
+		
+		one.operation?.cancel()
+		two.operation?.cancel()
+		three.operation?.cancel()
+		
+		// Wait for image to load from network
+		waitForExpectationsWithTimeout(100.0, handler:nil)
+	}
+	
 }
