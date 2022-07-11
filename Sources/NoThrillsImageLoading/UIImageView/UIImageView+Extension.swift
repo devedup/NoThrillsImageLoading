e53b1a2fdf4fded7d9bb1a4cbbe8e0d4ecb709e9
DaveC @ DevedUp <email@devedup.com>
2022-07-11 12:43:52 +0100

    Removed the activity indicators

--- Sources/NoThrillsImageLoading/UIImageView/UIImageView+Extension.swift
+++ Sources/NoThrillsImageLoading/UIImageView/UIImageView+Extension.swift
@@ -27,42 +27,11 @@ extension UIView {
 }
 
 public extension UIImageView {
-    
-    private struct AssociatedKeys {
-        static var ActivityIndicator = "ActivityIndicator"
-    }
 
-    private func presentActivityIndicator(style: UIActivityIndicatorView.Style = .gray) {
-        dismissActivityIndicator()
-        let view: UIView! = self
-
-        let activityIndicator =  UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-        activityIndicator.center = self.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.style = style
-        view.addSubview(activityIndicator)
-
-        activityIndicator.centreInSuperview()
-        view.bringSubviewToFront(activityIndicator)
-        activityIndicator.startAnimating()
-        objc_setAssociatedObject(self,
-                                 &AssociatedKeys.ActivityIndicator,
-                                 activityIndicator as UIActivityIndicatorView?,
-                                 .OBJC_ASSOCIATION_RETAIN_NONATOMIC )
-    }
-    
-    private func dismissActivityIndicator() {
-        if let activityIndicator = objc_getAssociatedObject(self, &AssociatedKeys.ActivityIndicator) as?  UIActivityIndicatorView {
-            activityIndicator.stopAnimating()
-        }
-    }
-    
     @discardableResult
     func loadFrom(url: URL, animationDuration: Double = 0.0, httpHeaders: [String: String]? = [:], priority: Priority = .normal, completion: ((UIImage?, Error?) -> Void)? = nil) -> Operation {
-        presentActivityIndicator()
         let headers = httpHeaders ?? [:]
         let operation = ImageCenter.imageForURL(url, httpHeaders: headers) { (result) in
-            self.dismissActivityIndicator()
             switch result {
             case .success(let result):
                 if result.url == url {
@@ -87,10 +56,8 @@ public extension UIImageView {
     
     @discardableResult
     func loadFromConfirm(url: URL, animationDuration: Double = 0.0, httpHeaders: [String: String]? = [:], completion: ((UIImage?, URL?, Error?) -> Void)? = nil) -> Operation {
-        presentActivityIndicator()
         let headers = httpHeaders ?? [:]
         let operation = ImageCenter.imageForURL(url, httpHeaders: headers) { (result) in
-            self.dismissActivityIndicator()
             switch result {
             case .success(let result):
                 if result.url == url {
